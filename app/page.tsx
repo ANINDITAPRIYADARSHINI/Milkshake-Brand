ef55840e14a3bba0339be0fe3826bee6dbea0ede
ANINDITAPRIYADARSHINI <anindita.priyadarshini8@gmail.com>
2025-02-19 22:04:55 +0530

    Two containers created.

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,14 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-row gap-14">
+        <div className="left">
+        hello
+        </div>
+        <div className="right">
+          hi
+        </div>
+      </div>
       
     </main>
   );
